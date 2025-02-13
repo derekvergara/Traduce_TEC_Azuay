664b2a5dec6517ec2323c371da47053e763e4801
MEJI-469 <mauriciomeji469@gmail.com>
2025-02-13 09:49:15 -0500

    historial
    cambios en las clases de hisorial

--- src/main/java/com/ista/demo/Controller/HitorialController.java
+++ src/main/java/com/ista/demo/Controller/HitorialController.java
@@ -1,8 +1,10 @@
 package com.ista.demo.Controller;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,26 +28,26 @@ public class HitorialController {
 
 	@Autowired
 	private IHistorialTraduccionService hitorialServ;
-	
+
 	@Autowired
-    private UsuarioDai usuarioDai; // Inyección de UsuarioDai
-	
+	private UsuarioDai usuarioDai; // Inyección de UsuarioDai
+
 	@GetMapping("/historial")
-	public List<Historial_traduccion> indext(){
+	public List<Historial_traduccion> indext() {
 		return hitorialServ.findAll();
 	}
-	
+
 	@GetMapping("/historial/{id}")
 	public Historial_traduccion show(@PathVariable Long id) {
 		return hitorialServ.findById(id);
 	}
-	
+
 	@PostMapping("/historial")
 	@ResponseStatus(HttpStatus.CREATED)
-	public Historial_traduccion create(@RequestBody Historial_traduccion histo){
+	public Historial_traduccion create(@RequestBody Historial_traduccion histo) {
 		return hitorialServ.save(histo);
 	}
-	
+
 	@PutMapping("/historial/{id}")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Historial_traduccion update(@RequestBody Historial_traduccion histo, @PathVariable Long id) {
@@ -52,37 +55,44 @@ public class HitorialController {
 		histo1.setTexto(histo.getTexto());
 		histo1.setTipo_traduccion(histo.getTipo_traduccion());
 		histo1.setFecha_traduccion(histo.getFecha_traduccion());
-		
+
 		return hitorialServ.save(histo1);
 	}
-	
+
 	@DeleteMapping("/historial/{id}")
 	public void delete(@PathVariable Long id) {
 		hitorialServ.delete(id);
 	}
-	
-	//Nuevo metodo para obtener las traducciones de un usuario
-		@GetMapping("/historial/usuario/{userId}")
-		public List<Historial_traduccion> getHistorialByUsuarioId(@PathVariable Long userId) {
-		    return hitorialServ.findByUsuarioId(userId);
-		}
-		
-		// Metodo para crear la traduccion con relacion al usuario
-		@PostMapping("/crear")
-	    @ResponseStatus(HttpStatus.CREATED)
-	    public Historial_traduccion crearHistorial(@RequestBody Historial_traduccion historial) {
-	        // Buscar el usuario por su ID
-	        Usuario usuario = usuarioDai.findById(historial.getUsuario().getId())
-	                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
-	        
-	        // Asociar el usuario al historial
-	        historial.setUsuario(usuario);
 
-	        // No envíes un ID manualmente
-	        historial.setId_historial_traduccion(null);
-	        
-	        // Guardar el historial
-	        return hitorialServ.save(historial);
-	    }
+	// Nuevo metodo para obtener las traducciones de un usuario
+	@GetMapping("/historial/usuario/{userId}")
+	public List<Historial_traduccion> getHistorialByUsuarioId(@PathVariable Long userId) {
+		return hitorialServ.findByUsuarioId(userId);
+	}
 
+	// Metodo para crear la traduccion con relacion al usuario
+	@PostMapping("/crear")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Historial_traduccion crearHistorial(@RequestBody Historial_traduccion historial) {
+		// Buscar el usuario por su ID
+		Usuario usuario = usuarioDai.findById(historial.getUsuario().getId())
+				.orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+		// Asociar el usuario al historial
+		historial.setUsuario(usuario);
+
+		// No envíes un ID manualmente
+		historial.setId_historial_traduccion(null);
+
+		// Guardar el historial
+		return hitorialServ.save(historial);
+	}
+
+	// Nuevo metodo de consutar por fechas
+	@GetMapping("/historial/usuario/{userId}/rango")
+	public List<Historial_traduccion> getHistorialByUsuarioAndDateRange(@PathVariable("userId") Long userId,
+			@RequestParam("start") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date start,
+			@RequestParam("end") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date end) {
+		return hitorialServ.findByUsuarioIdAndFechaBetween(userId, start, end);
+	}
 }
